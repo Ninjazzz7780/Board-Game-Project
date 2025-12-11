429dac9df4aac5c6573ef793aa4f1c921213dde3
Rafi Putra N <bartienayar04@gmail.com>
2025-12-11 14:00:11 +0700

    Membuat kode player queue untuk mengatur giliran player

--- src/PlayerQueue.java
+++ src/PlayerQueue.java
@@ -1,2 +1,35 @@
-public class PlayerQueue {
+// File: PlayerQueue.java
+import java.util.LinkedList;
+import java.util.Queue;
+
+class PlayerQueue {
+    private LinkedList<Player> queue;
+
+    public PlayerQueue() {
+        this.queue = new LinkedList<>();
+    }
+
+    public void enqueue(Player player) {
+        queue.offer(player);
+    }
+
+    public Player poll() {
+        return queue.poll();
+    }
+
+    public Player peek() {
+        return queue.peek();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public LinkedList<Player> getQueue() {
+        return queue;
+    }
 }
