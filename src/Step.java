c692bd10feb20e1262da8a4d9789a1bfbac775f9
Rafi Putra N <bartienayar04@gmail.com>
2025-12-11 14:20:42 +0700

    Membuat kode Step untuk Mencatat Riwayat atau Histori Gerakan (Move History Log) untuk setiap pemain.

--- src/Step.java
+++ src/Step.java
@@ -1,2 +1,23 @@
-public class Step {
+class Step {
+    private String direction;
+    private int steps;
+    private int position;
+
+    public Step(String direction, int steps, int position) {
+        this.direction = direction;
+        this.steps = steps;
+        this.position = position;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
